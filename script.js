fbdf7fd74e7cf234c786a2e66558a68720430da0
cllg-stud-raghav <raghav_bca23_26@its.edu.in>
2025-08-12 22:06:10 +0530

    added responsiveness,filters and CORS

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
+let cardsArr;
 try {
-    const response = await fetch('/api')
-    const data = await response.json()
-    renderCards(data)
+  const response = await fetch("/api");
+  const data = await response.json();
+  cardsArr=data;
+  renderCards(data);
 } catch (err) {
-    console.log(err)
+  console.log(err);
 }
-const renderAll = document.getElementsByClassName('nav-item')[0]
-const renderActive = document.getElementsByClassName('nav-item')[1]
-const renderInActive = document.getElementsByClassName('nav-item')[2]
-const removeBtnList = document.querySelectorAll('.remove');
-const ActiveBtnList = document.querySelectorAll('.ActiveBtn');
-const themeToggle=document.querySelector('.theme-toggle');
-const body=document.body;
+const renderAll = document.getElementsByClassName("nav-item")[0];
+const renderActive = document.getElementsByClassName("nav-item")[1];
+const renderInActive = document.getElementsByClassName("nav-item")[2];
+const removeBtnList = document.querySelectorAll(".remove");
+const ActiveBtnList = document.querySelectorAll(".ActiveBtn");
+const themeToggle = document.querySelector(".theme-toggle");
+const body = document.body;
 // console.log(removeBtn)
 
 let isActive = false;
 let isInActive = false;
-let isLight=true;
-
+let isLight = true;
+let isCardActive=false;
 
 //Event Handlers
 function renderCards(data) {
-    const cardContainer = document.getElementsByClassName('cards-container')[0];
-    let cardsHTML = '';
-    data.forEach((card) => {
-        cardsHTML += `
+  const cardContainer = document.querySelector(".cards-container");
+  let cardsHTML = "";
+  data.forEach((card) => {
+    cardsHTML += `
         <article class='card-article'>
         <div class='card-text'>
         <img src=${card.logo} alt=${card.name} />
@@ -40,64 +42,83 @@ function renderCards(data) {
          <span class="slider"></span>
         </label>
         </div>
-        </article>`
-    })
-    cardContainer.innerHTML = cardsHTML;
+        </article>`;
+  });
+  cardContainer.innerHTML = cardsHTML;
+  console.log('cards rendered');
 }
 
-
-
 // Event Listeners
-window.addEventListener('load', () => {
-    renderAll.classList.add('onActive');
-    // console.log('content loading');
+
+/* when windows load */
+window.addEventListener("load", () => {
+  renderAll.classList.add("onActive");
+  // console.log('content loading');
+});
+
+renderAll.addEventListener("click", () => {
+  if (isActive || isInActive) {
+    isInActive = false;
+    isActive = false;
+    renderActive.classList.remove("onActive");
+    renderInActive.classList.remove("onActive");
+    renderAll.classList.add("onActive");
+  }
+});
+renderAll.addEventListener('click',()=>{
+  renderCards(cardsArr);
 })
-renderAll.addEventListener('click', () => {
-    if (isActive || isInActive) {
-        isInActive = false;
-        isActive = false;
-        renderActive.classList.remove('onActive');
-        renderInActive.classList.remove('onActive');
-        renderAll.classList.add('onActive');
-    }
+renderActive.addEventListener("click", () => {
+  if (!isActive) {
+    isActive = true;
+    isInActive = false;
+    renderInActive.classList.remove("onActive");
+    renderAll.classList.remove("onActive");
+    renderActive.classList.add("onActive");
+  }
+});
+renderActive.addEventListener('click',()=>{
+  const filterArr=cardsArr.filter((card)=>{
+    return card.isActive===true;
+  })
+  renderCards(filterArr);
 })
-renderActive.addEventListener('click', () => {
-    if (!isActive) {
-        isActive = true;
-        isInActive = false
-        renderInActive.classList.remove('onActive');
-        renderAll.classList.remove('onActive');
-        renderActive.classList.add('onActive');
-    }
-})
-renderInActive.addEventListener('click', () => {
-    if (!isInActive) {
-        isActive = false;
-        isInActive = true;
-        renderActive.classList.remove('onActive');
-        renderAll.classList.remove('onActive');
-        renderInActive.classList.add('onActive');
-    }
+renderInActive.addEventListener("click", () => {
+  if (!isInActive) {
+    isActive = false;
+    isInActive = true;
+    renderActive.classList.remove("onActive");
+    renderAll.classList.remove("onActive");
+    renderInActive.classList.add("onActive");
+  }
+});
+renderInActive.addEventListener('click',()=>{
+  const filterArr=cardsArr.filter((card)=>{
+    return !(card.isActive===true);
+  })
+  renderCards(filterArr);
 })
 removeBtnList.forEach((item, inx) => {
-    const articleToRemove = item.parentElement.parentElement;
-    item.addEventListener('click', () => {
-        console.log('element deleted');
-        articleToRemove.style.display = 'none'
-    })
-})
-ActiveBtnList.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        
-    })
-})
-themeToggle.addEventListener('click',()=>{
-    body.classList.toggle('dark')
-    if(isLight){
-        isLight=false;
-        themeToggle.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#FBFDFE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`
-    }else{
-        isLight=true;
-        themeToggle.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#091540" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`
+  const articleToRemove = item.parentElement.parentElement;
+  item.addEventListener("click", () => {
+    console.log("element deleted");
+    articleToRemove.style.display = "none";
+  });
+});
+ActiveBtnList.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if(isActive){
+        console.log(e.textContent);
     }
-})
+  });
+});
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (isLight) {
+    isLight = false;
+    themeToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#FBFDFE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`;
+  } else {
+    isLight = true;
+    themeToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#091540" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`;
+  }
+});
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-import http from 'http'
-import serveStatic from './utils/serveStatic.js'
-import handleGet from './routers/handleGet.js'
+import http from "http";
+import serveStatic from "./utils/serveStatic.js";
+import handleGet from "./routers/handleGet.js";
 
 const port = 8000;
 
 const server = http.createServer(async (req, res) => {
-    console.log(`req: ${req.url}`)
-    if(!(req.url==='/api')){
-        await serveStatic(req,res)
-    }
-    else if(req.url==='/api' && req.method==="GET"){
-        await handleGet(res);
-    }
-})
-server.listen(port, () => console.log('server started at http://localhost:8000'))
+  console.log(`req: ${req.url}`);
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all domains
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (!(req.url === "/api")) {
+    await serveStatic(req, res);
+  } else if (req.url === "/api" && req.method === "GET") {
+    await handleGet(res);
+  }
+});
+server.listen(port, () =>
+  console.log("server started at http://localhost:8000")
+);
