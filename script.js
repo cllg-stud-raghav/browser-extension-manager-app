fc388cc1a3d87df176872d5a48381500427257ea
cllg-stud-Raghav <raghav_bca23_26@its.edu.in>
2025-08-14 20:58:18 +0530

    remove button now works with the filter cards array

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 let cardsArr;
-try {
-  const response = await fetch("/api");
-  const data = await response.json();
-  cardsArr=data;
-  renderCards(data);
-} catch (err) {
-  console.log(err);
-}
 const renderAll = document.getElementsByClassName("nav-item")[0];
 const renderActive = document.getElementsByClassName("nav-item")[1];
 const renderInActive = document.getElementsByClassName("nav-item")[2];
-const removeBtnList = document.querySelectorAll(".remove");
 const ActiveBtnList = document.querySelectorAll(".ActiveBtn");
 const themeToggle = document.querySelector(".theme-toggle");
 const body = document.body;
+try {
+    const response = await fetch("/api");
+    const data = await response.json();
+    cardsArr = data;
+    renderCards(data);
+} catch (err) {
+    console.log(err);
+}
 // console.log(removeBtn)
 
 let isActive = false;
 let isInActive = false;
 let isLight = true;
-let isCardActive=false;
+let isCardActive = false;
 
 //Event Handlers
 function renderCards(data) {
-  const cardContainer = document.querySelector(".cards-container");
-  let cardsHTML = "";
-  data.forEach((card) => {
-    cardsHTML += `
+    const cardContainer = document.querySelector(".cards-container");
+    let cardsHTML = "";
+    data.forEach((card) => {
+        cardsHTML += `
         <article class='card-article'>
         <div class='card-text'>
         <img src=${card.logo} alt=${card.name} />
@@ -38,87 +37,101 @@ function renderCards(data) {
         <div class='card-controls'>
         <button class='remove btn'>Remove</button>
         <label class="switch">
-         <input type="checkbox" class="ActiveBtn">
-         <span class="slider"></span>
+        <input type="checkbox" class="ActiveBtn">
+        <span class="slider"></span>
         </label>
         </div>
         </article>`;
-  });
-  cardContainer.innerHTML = cardsHTML;
-  console.log('cards rendered');
+    });
+    cardContainer.innerHTML = cardsHTML;
+    console.log('cards rendered');
+
+    
+    const removeBtnList = document.querySelectorAll(".remove");
+    removeBtnList.forEach((item) => {
+    const articleToRemove = item.parentElement.parentElement;
+    item.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("element deleted");
+        articleToRemove.style.display = "none";
+    });
+});
 }
 
 // Event Listeners
 
 /* when windows load */
-window.addEventListener("load", () => {
-  renderAll.classList.add("onActive");
-  // console.log('content loading');
+window.addEventListener("load", (e) => {
+    e.preventDefault();
+    renderAll.classList.add("onActive");
+    // console.log('content loading');
 });
 
-renderAll.addEventListener("click", () => {
-  if (isActive || isInActive) {
-    isInActive = false;
-    isActive = false;
-    renderActive.classList.remove("onActive");
-    renderInActive.classList.remove("onActive");
-    renderAll.classList.add("onActive");
-  }
-});
-renderAll.addEventListener('click',()=>{
-  renderCards(cardsArr);
-})
-renderActive.addEventListener("click", () => {
-  if (!isActive) {
-    isActive = true;
-    isInActive = false;
-    renderInActive.classList.remove("onActive");
-    renderAll.classList.remove("onActive");
-    renderActive.classList.add("onActive");
-  }
-});
-renderActive.addEventListener('click',()=>{
-  const filterArr=cardsArr.filter((card)=>{
-    return card.isActive===true;
-  })
-  renderCards(filterArr);
-})
-renderInActive.addEventListener("click", () => {
-  if (!isInActive) {
-    isActive = false;
-    isInActive = true;
-    renderActive.classList.remove("onActive");
-    renderAll.classList.remove("onActive");
-    renderInActive.classList.add("onActive");
-  }
-});
-renderInActive.addEventListener('click',()=>{
-  const filterArr=cardsArr.filter((card)=>{
-    return !(card.isActive===true);
-  })
-  renderCards(filterArr);
-})
-removeBtnList.forEach((item, inx) => {
-  const articleToRemove = item.parentElement.parentElement;
-  item.addEventListener("click", () => {
-    console.log("element deleted");
-    articleToRemove.style.display = "none";
-  });
-});
-ActiveBtnList.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    if(isActive){
-        console.log(e.textContent);
+renderAll.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (isActive || isInActive) {
+        isInActive = false;
+        isActive = false;
+        renderActive.classList.remove("onActive");
+        renderInActive.classList.remove("onActive");
+        renderAll.classList.add("onActive");
     }
-  });
 });
-themeToggle.addEventListener("click", () => {
-  body.classList.toggle("dark");
-  if (isLight) {
-    isLight = false;
-    themeToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#FBFDFE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`;
-  } else {
-    isLight = true;
-    themeToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#091540" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`;
-  }
+renderAll.addEventListener('click', (e) => {
+    e.preventDefault();
+    renderCards(cardsArr);
+})
+renderActive.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!isActive) {
+        isActive = true;
+        isInActive = false;
+        renderInActive.classList.remove("onActive");
+        renderAll.classList.remove("onActive");
+        renderActive.classList.add("onActive");
+    }
+});
+renderActive.addEventListener('click', (e) => {
+    e.preventDefault();
+    const filterArr = cardsArr.filter((card) => {
+        return card.isActive === true;
+    })
+    renderCards(filterArr);
+})
+renderInActive.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!isInActive) {
+        isActive = false;
+        isInActive = true;
+        renderActive.classList.remove("onActive");
+        renderAll.classList.remove("onActive");
+        renderInActive.classList.add("onActive");
+    }
+});
+renderInActive.addEventListener('click', (e) => {
+    e.preventDefault();
+    const filterArr = cardsArr.filter((card) => {
+        return !(card.isActive === true);
+    })
+    renderCards(filterArr);
+})
+
+
+// ActiveBtnList.forEach((item) => {
+//     item.addEventListener("click", (e) => {
+//         if (isActive) {
+//             console.log(e.textContent);
+//         }
+//     });
+// });
+themeToggle.addEventListener("click", (e) => {
+    e.preventDefault();
+    body.classList.toggle("dark");
+    if (isLight) {
+        isLight = false;
+        themeToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#FBFDFE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M11 1.833v1.834m0 14.666v1.834M3.667 11H1.833m3.955-5.212L4.492 4.492m11.72 1.296 1.297-1.296M5.788 16.215l-1.296 1.296m11.72-1.296 1.297 1.296M20.167 11h-1.834m-2.75 0a4.583 4.583 0 1 1-9.167 0 4.583 4.583 0 0 1 9.167 0Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`;
+    } else {
+        isLight = true;
+        themeToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 22 22"><g clip-path="url(#a)"><path stroke="#091540" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.98" d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h22v22H0z"/></clipPath></defs></svg>`;
+    }
 });
